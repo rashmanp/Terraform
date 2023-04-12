84451d6ce6dee30d23ed1de4b72965417423b94e
rashmanp <32740633+rashmanp@users.noreply.github.com>
2023-04-12 13:33:39 +0530

    Update main.tf

--- buildingblock1/main.tf
+++ buildingblock1/main.tf
@@ -1,4 +1,8 @@
-resource "aws_s3_bucket" "test"
-{
-  acl = "Private"
+resource "aws_s3_bucket" "example" {
+  bucket = "my-tf-test-bucket"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
 }
